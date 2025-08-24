49300a7368367db8b1b0fc79ae72604a1bc9a8d9
Mark Philpot <mark.philpot@gmail.com>
2025-08-23 18:57:40 -0700

    Fix entities

--- src/app/invest/page.tsx
+++ src/app/invest/page.tsx
@@ -3,11 +3,6 @@ import Section from "@/components/Section";
 export default function Investor() {
   return (
     <Section>
-      {/*<p className="lead">*/}
-      {/*  Until now, trying to style an article, document, or blog post with*/}
-      {/*  Tailwind has been a tedious task that required a keen eye for typography*/}
-      {/*  and a lot of complex custom CSS.*/}
-      {/*</p>*/}
       <p>
         By default, Tailwind removes all of the default browser styling from
         paragraphs, headings, lists and more. This ends up being really useful
@@ -28,8 +23,8 @@ export default function Investor() {
         </p>
       </blockquote>
       <p>
-        We hear you, but we're not convinced that simply disabling our base
-        styles is what you really want. You don't want to have to remove
+        We hear you, but we&apos;re not convinced that simply disabling our base
+        styles is what you really want. You don&#39;t want to have to remove
         annoying margins every time you use a <code>p</code> element in a piece
         of your dashboard UI. And I doubt you really want your blog posts to use
         the user-agent styles either — you want them to look <em>awesome</em>,
@@ -56,38 +51,42 @@ export default function Investor() {
       <hr />
       <h2>What to expect from here on out</h2>
       <p>
-        What follows from here is just a bunch of absolute nonsense I've written
-        to dogfood the plugin itself. It includes every sensible typographic
-        element I could think of, like <strong>bold text</strong>, unordered
-        lists, ordered lists, code blocks, block quotes,{" "}
+        What follows from here is just a bunch of absolute nonsense I&apos;ve
+        written to dogfood the plugin itself. It includes every sensible
+        typographic element I could think of, like <strong>bold text</strong>,
+        unordered lists, ordered lists, code blocks, block quotes,{" "}
         <em>and even italics</em>.
       </p>
-      <p>It's important to cover all of these use cases for a few reasons:</p>
+      <p>
+        It&apos;s important to cover all of these use cases for a few reasons:
+      </p>
       <ol>
         <li>We want everything to look good out of the box.</li>
         <li>
-          Really just the first reason, that's the whole point of the plugin.
+          Really just the first reason, that&apos;s the whole point of the
+          plugin.
         </li>
         <li>
-          Here's a third pretend reason though a list with three items looks
-          more realistic than a list with two items.
+          Here&apos;s a third pretend reason though a list with three items
+          looks more realistic than a list with two items.
         </li>
       </ol>
-      <p>Now we're going to try out another header style.</p>
+      <p>Now we&apos;re going to try out another header style.</p>
       <h3>Typography should be easy</h3>
       <p>
-        So that's a header for you — with any luck if we've done our job
-        correctly that will look pretty reasonable.
+        So that&apos;s a header for you — with any luck if we&apos;ve done our
+        job correctly that will look pretty reasonable.
       </p>
       <p>Something a wise person once told me about typography is:</p>
       <blockquote>
         <p>
-          Typography is pretty important if you don't want your stuff to look
-          like trash. Make it good then it won't be bad.
+          Typography is pretty important if you don&apos;t want your stuff to
+          look like trash. Make it good then it won&apos;t be bad.
         </p>
       </blockquote>
       <p>
-        It's probably important that images look okay here by default as well:
+        It&apos;s probably important that images look okay here by default as
+        well:
       </p>
       <figure>
         <img
@@ -101,15 +100,15 @@ export default function Investor() {
         </figcaption>
       </figure>
       <p>
-        Now I'm going to show you an example of an unordered list to make sure
-        that looks good, too:
+        Now I&apos;m going to show you an example of an unordered list to make
+        sure that looks good, too:
       </p>
       <ul>
         <li>So here is the first item in this list.</li>
-        <li>In this example we're keeping the items short.</li>
-        <li>Later, we'll use longer, more complex list items.</li>
+        <li>In this example we&apos;re keeping the items short.</li>
+        <li>Later, we&apos;ll use longer, more complex list items.</li>
       </ul>
-      <p>And that's the end of this section.</p>
+      <p>And that&apos;s the end of this section.</p>
       <h2>What if we stack headings?</h2>
       <h3>We should make sure that looks good, too.</h3>
       <p>
@@ -121,8 +120,8 @@ export default function Investor() {
       <h3>When a heading comes after a paragraph …</h3>
       <p>
         When a heading comes after a paragraph, we need a bit more space, like I
-        already mentioned above. Now let's see what a more complex list would
-        look like.
+        already mentioned above. Now let&apos;s see what a more complex list
+        would look like.
       </p>
       <ul>
         <li>
@@ -133,14 +132,14 @@ export default function Investor() {
           </p>
           <p>
             For some reason I think this looks cool which is unfortunate because
-            it's pretty annoying to get the styles right.
+            it&apos;s pretty annoying to get the styles right.
           </p>
           <p>
             I often have two or three paragraphs in these list items, too, so
             the hard part is getting the spacing between the paragraphs, list
             item heading, and separate list items to all make sense. Pretty
             tough honestly, you could make a strong argument that you just
-            shouldn't write this way.
+            shouldn&apos;t write this way.
           </p>
         </li>
         <li>
@@ -148,19 +147,22 @@ export default function Investor() {
             <strong>Since this is a list, I need at least two items.</strong>
           </p>
           <p>
-            I explained what I'm doing already in the previous list item, but a
-            list wouldn't be a list if it only had one item, and we really want
-            this to look realistic. That's why I've added this second list item
-            so I actually have something to look at when writing the styles.
+            I explained what I&apos;m doing already in the previous list item,
+            but a list wouldn&apos;t be a list if it only had one item, and we
+            really want this to look realistic. That&apos;s why I&apos;ve added
+            this second list item so I actually have something to look at when
+            writing the styles.
           </p>
         </li>
         <li>
           <p>
-            <strong>It's not a bad idea to add a third item either.</strong>
+            <strong>
+              It&apos;s not a bad idea to add a third item either.
+            </strong>
           </p>
           <p>
-            I think it probably would've been fine to just use two items but
-            three is definitely not worse, and since I seem to be having no
+            I think it probably would&apos;ve been fine to just use two items
+            but three is definitely not worse, and since I seem to be having no
             trouble making up arbitrary things to type, I might as well include
             it.
           </p>
@@ -175,28 +177,29 @@ export default function Investor() {
         I think most people are going to use{" "}
         <a href="https://highlightjs.org/">highlight.js</a> or{" "}
         <a href="https://prismjs.com/">Prism</a> or something if they want to
-        style their code blocks but it wouldn't hurt to make them look{" "}
+        style their code blocks but it wouldn&apos;t hurt to make them look{" "}
         <em>okay</em> out of the box, even with no syntax highlighting.
       </p>
       <p>
-        Here's what a default <code>tailwind.config.js</code> file looks like at
-        the time of writing:
+        Here&apos;s what a default <code>tailwind.config.js</code> file looks
+        like at the time of writing:
       </p>
       <p>Hopefully that looks good enough to you.</p>
       <h3>What about nested lists?</h3>
       <p>
         Nested lists basically always look bad which is why editors like Medium
-        don't even let you do it, but I guess since some of you goofballs are
-        going to do it we have to carry the burden of at least making it work.
+        don&apos;t even let you do it, but I guess since some of you goofballs
+        are going to do it we have to carry the burden of at least making it
+        work.
       </p>
       <ol>
         <li>
           <strong>Nested lists are rarely a good idea.</strong>
           <ul>
             <li>
-              You might feel like you are being really "organized" or something
-              but you are just creating a gross shape on the screen that is hard
-              to read.
+              You might feel like you are being really &quot;organized&quot; or
+              something but you are just creating a gross shape on the screen
+              that is hard to read.
             </li>
             <li>
               Nested navigation in UIs is a bad idea too, keep things as flat as
@@ -208,10 +211,13 @@ export default function Investor() {
           </ul>
         </li>
         <li>
-          <strong>Since we need to have more items, here's another one.</strong>
+          <strong>
+            Since we need to have more items, here&apos;s another one.
+          </strong>
           <ul>
             <li>
-              I'm not sure if we'll bother styling more than two levels deep.
+              I&apos;m not sure if we&apos;ll bother styling more than two
+              levels deep.
             </li>
             <li>
               Two is already too much, three is guaranteed to be a bad idea.
@@ -220,20 +226,22 @@ export default function Investor() {
           </ul>
         </li>
         <li>
-          <strong>Two items isn't really a list, three is good though.</strong>
+          <strong>
+            Two items isn&apos;t really a list, three is good though.
+          </strong>
           <ul>
             <li>
-              Again please don't nest lists if you want people to actually read
-              your content.
+              Again please don&apos;t nest lists if you want people to actually
+              read your content.
             </li>
             <li>Nobody wants to look at this.</li>
-            <li>I'm upset that we even have to bother styling this.</li>
+            <li>I&apos;m upset that we even have to bother styling this.</li>
           </ul>
         </li>
       </ol>
       <p>
         The most annoying thing about lists in Markdown is that{" "}
-        <code>&lt;li&gt;</code> elements aren't given a child{" "}
+        <code>&lt;li&gt;</code> elements aren&apos;t given a child{" "}
         <code>&lt;p&gt;</code> tag unless there are multiple paragraphs in the
         list item. That means I have to worry about styling that annoying
         situation too.
@@ -241,12 +249,12 @@ export default function Investor() {
       <ul>
         <li>
           <p>
-            <strong>For example, here's another nested list.</strong>
+            <strong>For example, here&apos;s another nested list.</strong>
           </p>
           <p>But this time with a second paragraph.</p>
           <ul>
             <li>
-              These list items won't have <code>&lt;p&gt;</code> tags
+              These list items won&apos;t have <code>&lt;p&gt;</code> tags
             </li>
             <li>Because they are only one line each</li>
           </ul>
@@ -262,13 +270,16 @@ export default function Investor() {
           <ul>
             <li>
               <p>
-                As you can see here, because I've added a second line, this list
-                item now has a <code>&lt;p&gt;</code> tag.
+                As you can see here, because I&apos;ve added a second line, this
+                list item now has a <code>&lt;p&gt;</code> tag.
               </p>
-              <p>This is the second line I'm talking about by the way.</p>
+              <p>This is the second line I&apos;m talking about by the way.</p>
             </li>
             <li>
-              <p>Finally here's another list item so it's more like a list.</p>
+              <p>
+                Finally here&apos;s another list item so it&apos;s more like a
+                list.
+              </p>
             </li>
           </ul>
         </li>
@@ -283,8 +294,8 @@ export default function Investor() {
         <a href="https://tailwindcss.com">
           this link to the Tailwind CSS website
         </a>
-        . We almost made them blue but that's so yesterday, so we went with dark
-        gray, feels edgier.
+        . We almost made them blue but that&apos;s so yesterday, so we went with
+        dark gray, feels edgier.
       </p>
       <p>We even included table styles, check it out:</p>
       <table>
@@ -297,7 +308,7 @@ export default function Investor() {
         </thead>
         <tbody>
           <tr>
-            <td>Bret "The Hitman" Hart</td>
+            <td>{'Bret "The Hitman" Hart'}</td>
             <td>Calgary, AB</td>
             <td>Sharpshooter</td>
           </tr>
@@ -319,7 +330,7 @@ export default function Investor() {
           <tr>
             <td>Razor Ramon</td>
             <td>Chuluota, FL</td>
-            <td>Razor's Edge</td>
+            <td>Razor&apos;s Edge</td>
           </tr>
         </tbody>
       </table>
@@ -332,54 +343,56 @@ export default function Investor() {
         Sometimes I even use <code>code</code> in headings
       </h3>
       <p>
-        Even though it's probably a bad idea, and historically I've had a hard
-        time making it look good. This{" "}
-        <em>"wrap the code blocks in backticks"</em> trick works pretty well
+        Even though it&apos;s probably a bad idea, and historically I&apos;ve
+        had a hard time making it look good. This{" "}
+        <em>{'"wrap the code blocks in backticks"'}</em> trick works pretty well
         though really.
       </p>
       <p>
-        Another thing I've done in the past is put a <code>code</code> tag
+        Another thing I&apos;ve done in the past is put a <code>code</code> tag
         inside of a link, like if I wanted to tell you about the{" "}
         <a href="https://github.com/tailwindcss/docs">
           <code>tailwindcss/docs</code>
         </a>{" "}
-        repository. I don't love that there is an underline below the backticks
-        but it is absolutely not worth the madness it would require to avoid it.
+        repository. I don&apos;t love that there is an underline below the
+        backticks but it is absolutely not worth the madness it would require to
+        avoid it.
       </p>
       <h4>
-        We haven't used an <code>h4</code> yet
+        We haven&apos;t used an <code>h4</code> yet
       </h4>
       <p>
-        But now we have. Please don't use <code>h5</code> or <code>h6</code> in
-        your content, Medium only supports two heading levels for a reason, you
-        animals. I honestly considered using a <code>before</code>{" "}
-        pseudo-element to scream at you if you use an <code>h5</code> or{" "}
-        <code>h6</code>.
+        But now we have. Please don&apos;t use <code>h5</code> or{" "}
+        <code>h6</code> in your content, Medium only supports two heading levels
+        for a reason, you animals. I honestly considered using a{" "}
+        <code>before</code> pseudo-element to scream at you if you use an{" "}
+        <code>h5</code> or <code>h6</code>.
       </p>
       <p>
-        We don't style them at all out of the box because <code>h4</code>{" "}
+        We don&apos;t style them at all out of the box because <code>h4</code>{" "}
         elements are already so small that they are the same size as the body
         copy. What are we supposed to do with an <code>h5</code>, make it{" "}
         <em>smaller</em> than the body copy? No thanks.
       </p>
       <h3>We still need to think about stacked headings though.</h3>
       <h4>
-        Let's make sure we don't screw that up with <code>h4</code> elements,
-        either.
+        Let&apos;s make sure we don&apos;t screw that up with <code>h4</code>{" "}
+        elements, either.
       </h4>
       <p>
         Phew, with any luck we have styled the headings above this text and they
         look pretty good.
       </p>
       <p>
-        Let's add a closing paragraph here so things end with a decently sized
-        block of text. I can't explain why I want things to end that way but I
-        have to assume it's because I think things will look weird or unbalanced
-        if there is a heading too close to the end of the document.
+        Let&apos;s add a closing paragraph here so things end with a decently
+        sized block of text. I can&apos;t explain why I want things to end that
+        way but I have to assume it&apos;s because I think things will look
+        weird or unbalanced if there is a heading too close to the end of the
+        document.
       </p>
       <p>
-        What I've written here is probably long enough, but adding this final
-        sentence can't hurt.
+        What I&apos;ve written here is probably long enough, but adding this
+        final sentence can&apos;t hurt.
       </p>
     </Section>
   );
